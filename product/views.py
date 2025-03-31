84986d21dba35a8fd0101007aeed1db5a88c2027
Andrii Chumak <andrii.chumak.shi.2023@lpnu.ua>
2025-03-31 18:51:26 +0300

    added restAPI

--- product/views.py
+++ product/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from product.repositories.category_repository import CategoryRepository
+from product.repositories.product_repository import ProductRepository
+from product.repositories.review_repository import ReviewRepository
+from product.serializers import ProductSerializer, CategorySerializer, ReviewSerializer
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = ProductRepository.get_all()
+    serializer_class = ProductSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = CategoryRepository.get_all()
+    serializer_class = CategorySerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = ReviewRepository.get_all()
+    serializer_class = ReviewSerializer
